97a7502446f222c83532a4d6927f1adcdc5c53cb
JG <grafjude@badinhs.org>
2016-09-30 08:00:22 -0400

    Initial Commit

---
+++ myFAVemoji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myFAVemoji
+//
+//  Created by BHSRam8 on 9/30/16.
+//  Copyright © 2016 Jude Graf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
