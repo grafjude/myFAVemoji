975a30e12b478153626a115c8fd9fedd6cbf827b
JG <grafjude@badinhs.org>
2016-10-03 08:25:12 -0400

    commit 2

--- myFAVemoji/ViewController.swift
+++ myFAVemoji/ViewController.swift
@@ -8,16 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var cooltableView: UITableView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        cooltableView.dataSource=self
+        //this is used to define the table
+        cooltableView.delegate=self
+        //and contents
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 100
+           //this tells xcode to look for the number of rows as an integer
+   
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell=UITableViewCell()
+        cell.textLabel?.text="😈"
+        return cell
+        //this tells xcode what we want in the cells in order
+    }
+ 
+    
+    
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
